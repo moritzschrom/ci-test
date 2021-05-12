f241e79082df78cf5c3181cad59fe21305a477bb
Moritz Schrom <moritz.schrom@schrom.co>
2021-05-12 21:50:30 +0200

    Added surefire plugin for native JUnit 5 support.

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        calculator = new Calculator();
+    }
+
+    @Test
+    @DisplayName("Simple multiplication should work")
+    public void testMultiply() {
+        assertEquals(20, calculator.multiply(4,5),
+                "Regular multiplication should work");
+    }
+
+}
